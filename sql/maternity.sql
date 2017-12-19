4a8c81eefa9649e91f23d8797d7cdb051a25d7eb
childhealth <mike.kelly1@nhs.net>
2017-12-19 13:24:36 +0000

    Create maternity.sql

---
+++ sql/maternity.sql
@@ -0,0 +1,16 @@
+/*
+ * SQL to create maternity tables.
+ * Assumes tables do not already exist.
+ * Based on Maternity Data Model Version 0.6
+ *
+ * MAK 19-12-2017: First version
+ */
+ 
+CREATE TABLE Boundary(
+  BoundaryID VARCHAR(20) NOT NULL,
+  Description VARCHAR(250) NOT NULL,
+  CONSTRAINT PK_Boundary PRIMARY KEY (BoundaryID)
+);
+
+CREATE INDEX idx_type
+ON Boundary (BoundaryID);
