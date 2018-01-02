aa54c28b234836b3beb17fe3558472cc25b1e55f
childhealth <mike.kelly1@nhs.net>
2018-01-02 13:40:07 +0000

    Update maternity.sql

--- sql/maternity.sql
+++ sql/maternity.sql
@@ -16,51 +16,110 @@ CREATE TABLE MaternityRecords(
 CREATE TABLE Codes(
 );
 
-/* AllergiesAdverseReactions - Stores allergies and adverse reactions associated with a maternity record
+/* AllergiesAdverseReactions - Stores allergies and adverse reactions associated with maternity records
  */
 CREATE TABLE AllergiesAdverseReactions(
 );
 
-/* Assessments - Stores assessment scales associated with a maternity record
+/* Assessments - Stores assessment scales associated with maternity records
  */
 CREATE TABLE Assessments(
 );
 
-/* Attendances - Stores attendance details associated with a maternity record
+/* Attendances - Stores attendance details associated with maternity records
  */
 CREATE TABLE Attendances(
 );
 
-/* ClinicalRiskFactors - Stores clinical risk factors associated with a maternity record
+/* ClinicalRiskFactors - Stores clinical risk factors associated with maternity records
  */
 CREATE TABLE ClinicalRiskFactors(
 };
 
-/* ConditionsDiagnoses - Stores conditions or diagnoses associated with a maternity record
+/* ConditionsDiagnoses - Stores conditions or diagnoses associated with maternity records
  */
 CREATE TABLE ConditionsDiagnoses(
 );
 
-/* Discharges - Stores discharge details associated with a maternity record
+/* Discharges - Stores discharge details associated with maternity records
  */
 CREATE TABLE Discharges(
 );
+
+/* Examinations - Stores examination findings associated with maternity records
+ */
+CREATE TABLE Examinations(
+);
  
-Examination Findings
-Family History - Maternity
-Fetal Anomaly - Down’s, Edwards’ and Patau’s Syndromes
-Fetal Anomaly - Structural Anomalies
-Immunisation Administration
-Individual Requirements
-Infectious Diseases Screening
-Information and Advice given
-Investigation Results
-Legal Information
-Measurements
-Pregnancy History
-Medication
-Person and carer concerns, expectations and wishes
-Personal Comment
+/* FamilyHistories - Stores family histories associated with maternity records
+ */
+CREATE TABLE FamilyHistories(
+);
+ 
+/* FetalAnomaliesSyndromes - Stores Down’s, Edwards’ and Patau’s Syndromes associated with maternity records
+ */
+CREATE TABLE FetalAnomaliesSyndromes(
+);
+ 
+/* FetalAnomaliesStructural - Stores structural anomalies associated with maternity records
+ */
+CREATE TABLE FetalAnomaliesStructural(
+);
+ 
+/* Immunisations - Stores immunisation administrations associated with maternity records
+ */
+CREATE TABLE Immunisations(
+);
+ 
+/* IndividualRequirements - Stores individual requirements associated with maternity records
+ */
+CREATE TABLE IndividualRequirements(
+);
+ 
+/* InfectiousDiseasesScreening - Stores infectious diseases screening associated with maternity records
+ */
+CREATE TABLE InfectiousDiseasesScreening(
+); 
+ 
+/* InformationAdvice - Stores information and advice given associated with maternity records
+ */
+CREATE TABLE InformationAdvice(
+); 
+ 
+/* Investigations - Stores investigation results associated with maternity records
+ */
+CREATE TABLE Investigations(
+);
+ 
+/* LegalInformation - Stores legal information associated with maternity records
+ */
+CREATE TABLE LegalInformation(
+);
+ 
+/* Measurements - Stores measurements associated with maternity records
+ */
+CREATE TABLE Measurements(
+);
+
+/* PregnancyHistories - Stores pregnancy histories associated with maternity records
+ */
+CREATE TABLE PregnancyHistories(
+); 
+
+/* Medications - Stores medications associated with maternity records
+ */
+CREATE TABLE Medications(
+);
+
+/* PersonalConcerns - Stores person and carer concerns, expectations and wishes associated with maternity records
+ */
+CREATE TABLE PersonalConcerns(
+)
+
+/* PersonalComments - Stores personal comments associated with maternity records
+ */
+ 
+ 
 Personal Contacts
 Plan & Requested Actions
 Pregnancy Episode Details
