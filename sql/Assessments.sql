ea8fd0625fd3c8a289349a44c20468685d93c855
childhealth <mike.kelly1@nhs.net>
2018-01-04 14:58:29 +0000

    Update Assessments.sql

--- sql/Assessments.sql
+++ sql/Assessments.sql
@@ -1,1 +1,11 @@
+/*
+ * SQL to create Assessmentss table.
+ * Stores allergies and adverse reactions associated with MaternityRecords.
+ * Assumes table does not already exist.
+ * Based on Maternity Data Model Version 0.6
+ *
+ * MAK 04-01-2018: First version
+ */
 
+/* Create table
+ */
