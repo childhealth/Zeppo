4bf1d17db4155c00f7654f75f65899a4da286216
childhealth <mike.kelly1@nhs.net>
2018-01-04 14:51:47 +0000

    Update AllergiesAdverseReactions.sql

--- sql/AllergiesAdverseReactions.sql
+++ sql/AllergiesAdverseReactions.sql
@@ -1,1 +1,7 @@
-
+/*
+ * SQL to create AllergiesAdverseReactions table.
+ * Assumes table does not already exist.
+ * Based on Maternity Data Model Version 0.6
+ *
+ * MAK 04-01-2018: First version
+ */
