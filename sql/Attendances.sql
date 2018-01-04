b020cd913ae9e0a1af89b13f9d751e0135be4ccd
childhealth <mike.kelly1@nhs.net>
2018-01-04 15:53:19 +0000

    Update Attendances.sql

--- sql/Attendances.sql
+++ sql/Attendances.sql
@@ -42,21 +42,33 @@ INSERT INTO CodeLists VALUES (11, 'Attendances', 'Outcome', 'SYSTEM', '2018-01-0
 
 /* Insert Attendances related codes
  */
-INSERT INTO Codes VALUES (16, 4, '0', 'No', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (17, 4, '1', 'Yes', 'SYSTEM', '2018-01-01 01:00:00'); 
+INSERT INTO Codes VALUES (62, 6, '1', 'Accident and Emergency Attendance', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (63, 6, '4', 'Cancer Clinical Status Assessment', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (64, 6, '5', 'Care Programme Approach Review', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (65, 6, '6', 'Clinic Attendance Consultant', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (66, 6, '8', 'Clinic Attendance Midwife', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (67, 6, '9', 'Clinic Attendance Non-Consultant', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (68, 6, '10', 'Clinic Attendance Nurse', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (69, 6, '14', 'Domiciliary Consultation', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (70, 6, '22', 'Sexual and Reproductive Health Domiciliary Visit', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (71, 6, '23', 'Genitourinary Consultant Clinic Attendance', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (72, 6, '27', 'Maternity Domiciliary Visit', 'SYSTEM', '2018-01-01 01:00:00');
 
-INSERT INTO Codes VALUES (18, 5, '1', 'Health of the Nation Outcome Scale (Working Age Adults)', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (19, 5, '3', 'Patient Health Questionnaire-9', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (20, 5, '4', 'Agoraphobia Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (21, 5, '5', 'Agoraphobia Mobility Inventory Questionnaire ''When Accompanied''', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (22, 5, '6', 'Agoraphobia Mobility Inventory Questionnaire ''When Alone''', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (23, 5, '7', 'Employment Status Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (24, 5, '8', 'Generalised Anxiety Disorder Penn State Worry Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (25, 5, '9', 'Generalised Anxiety Disorder Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (26, 5, '10', 'Health Anxiety Inventory Short Week Scale', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (27, 5, '11', 'Obsessive Compulsive Disorder Inventory Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (28, 5, '12', 'Panic Disorder Severity Scale', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (29, 5, '13', 'Post Traumatic Stress Disorder Impacts of Events Revised Scale', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (30, 5, '14', 'Social Phobia Inventory Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (31, 5, '15', 'Social Phobia Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (32, 5, '16', 'Specific Phobia Questionnaire', 'SYSTEM', '2018-01-01 01:00:00');
+/*
+29	Nurse or Midwife Contact
+30	Out-Patient Attendance Consultant
+38	Theatre Case
+39	Ward Attendance
+41	Improving Access to Psychological Therapies Contact
+42	NHS Health Check Assessment
+43	Antenatal Booking Appointment
+44	Pregnancy First Contact
+45	Nutritional Assessment
+46	HIV Clinic Attendance
+47	Multi-Disciplinary Consultation (National Tariff Payment System)
+48	Multi-Professional Consultation (National Tariff Payment System)
+49	Two Year Neonatal Outcomes Assessment
+50	Radiotherapy Attendance
+51	Holistic Needs Assessment
+52	Emergency Care Attendance
+*/
