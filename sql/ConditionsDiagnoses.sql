23237f7579412b20456018ea75b27832a562ceaf
childhealth <mike.kelly1@nhs.net>
2018-01-05 11:39:27 +0000

    Update ConditionsDiagnoses.sql

--- sql/ConditionsDiagnoses.sql
+++ sql/ConditionsDiagnoses.sql
@@ -1,7 +1,7 @@
 
 /*
  * SQL to create ConditionsDiagnoses table.
- * Stores attendance details associated with maternity records.
+ * Stores conditions or diagnoses associated with maternity records.
  * Assumes table does not already exist.
  * Based on Maternity Data Model Version 0.6
  *
@@ -11,33 +11,23 @@
 /* Create table
  */
 CREATE TABLE ConditionsDiagnoses (
- AttendanceID INTEGER NOT NULL,
+ ConditionDiagnosisID INTEGER NOT NULL,
  MaternityRecordID INTEGER NOT NULL,
-  
- DateRecorded DATE NOT NULL,
- LocationRecorded TEXT NOT NULL,
- -- PerformerID ???
- PerformerName TEXT NOT NULL,
- PerformerJobRole TEXT NOT NULL,        -- Should be coded
- Indication TEXT;
- ContactType VARCHAR(2),                -- Coded
- ConsultationMethod VARCHAR(2),         -- Coded
- Speciality VARCHAR(3),                 -- Coded
- Service VARCHAR(2),                    -- Coded
- -- Care professionals present 0..*
- PersonAccompanyingPatient VARCHAR(10), -- Coded
- Outcome VARCHAR(5),                    -- Coded
-  
+ Condition VARCHAR(18) NOT NULL; -- Coded
+ Category VARCHAR(1) NOT NULL,   -- Coded
+ Stage TEXT,
+ Onset DATE,
  UStamp VARCHAR(20) NOT NULL,
  DTStamp TIMESTAMP NOT NULL,
- CONSTRAINT PK_Attendances PRIMARY KEY (AttendanceID),
- CONSTRAINT FK_Attendances_MaternityRecords FOREIGN KEY (MaternityRecordID) REFERENCES MaternityRecords(MaternityRecordID)
+ CONSTRAINT PK_ConditionsDiagnoses PRIMARY KEY (ConditionDiagnosisID),
+ CONSTRAINT FK_ConditionsDiagnoses_MaternityRecords FOREIGN KEY (MaternityRecordID) REFERENCES MaternityRecords(MaternityRecordID)
 );
 
-/* Insert Attendances related codelists
+/* Insert ConditionsDiagnoses related codelists
  */
-INSERT INTO CodeLists VALUES (12, 'Attendances', 'ContactType', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO CodeLists VALUES (12, 'ConditionsDiagnoses', 'Condition', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO CodeLists VALUES (13, 'ConditionsDiagnoses', 'Category', 'SYSTEM', '2018-01-01 01:00:00');
 
-/* Insert Attendances related codes
+/* Insert ConditionsDiagnoses related codes
  */
-INSERT INTO Codes VALUES (62, 12, '1', 'Accident and Emergency Attendance', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (62, 12, '1', 'X', 'SYSTEM', '2018-01-01 01:00:00');
