fff90ee14e5412a594dd5964556cfe6ca66f0b75
childhealth <mike.kelly1@nhs.net>
2018-01-05 11:22:32 +0000

    Update ClinicalRiskFactors.sql

--- sql/ClinicalRiskFactors.sql
+++ sql/ClinicalRiskFactors.sql
@@ -1,6 +1,6 @@
 /*
  * SQL to create ClinicalRiskFactors table.
- * Stores attendance details associated with maternity records.
+ * Stores clinical risk factors associated with maternity records.
  * Assumes table does not already exist.
  * Based on Maternity Data Model Version 0.6
  *
@@ -12,21 +12,14 @@
 CREATE TABLE ClinicalRiskFactors (
  ClinicalRiskFactorID INTEGER NOT NULL,
  MaternityRecordID INTEGER NOT NULL,
-  
  DateRecorded DATE NOT NULL,
  LocationRecorded TEXT NOT NULL,
  -- PerformerID ???
  PerformerName TEXT NOT NULL,
  PerformerJobRole TEXT NOT NULL,        -- Should be coded
- Indication TEXT;
- ContactType VARCHAR(2),                -- Coded
- ConsultationMethod VARCHAR(2),         -- Coded
- Speciality VARCHAR(3),                 -- Coded
- Service VARCHAR(2),                    -- Coded
- -- Care professionals present 0..*
- PersonAccompanyingPatient VARCHAR(10), -- Coded
- Outcome VARCHAR(5),                    -- Coded
-  
+ ClinicalRiskFactor TEXT NOT NULL;
+ RiskAssessment TEXT NOT NULL,
+ RiskMitigation TEXT NOT NULL,
  UStamp VARCHAR(20) NOT NULL,
  DTStamp TIMESTAMP NOT NULL,
  CONSTRAINT PK_ClinicalRiskFactors PRIMARY KEY (ClinicalRiskFactorID),
