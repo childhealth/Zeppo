d695e5e304c432e9bca6fd3ea83b79dec04cc552
childhealth <mike.kelly1@nhs.net>
2018-01-05 13:56:41 +0000

    Update Discharges.sql

--- sql/Discharges.sql
+++ sql/Discharges.sql
@@ -36,20 +36,16 @@ INSERT INTO CodeLists VALUES (15, 'Discharges', 'Destination', 'SYSTEM', '2018-0
 /* Insert Discharges related codes
  */
 INSERT INTO Codes VALUES (310, 14, '1', 'PATIENT discharged on clinical advice or with clinical consent', 'SYSTEM', '2018-01-01 01:00:00');
-INSERT INTO Codes VALUES (310, 14, '2', 'PATIENT discharged him/herself or was discharged by a relative or advocate', 'SYSTEM', '2018-01-01 01:00:00');
-3	PATIENT discharged by mental health review tribunal, Home Secretary or Court
-INSERT INTO Codes VALUES (310, 14, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
-4	PATIENT died
-INSERT INTO Codes VALUES (310, 14, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
-5	Stillbirth
-INSERT INTO Codes VALUES (310, 14, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
-6	PATIENT discharged him/herself *
-INSERT INTO Codes VALUES (310, 14, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
-7	PATIENT discharged by a relative or advocate *
-INSERT INTO Codes VALUES (310, 14, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (311, 14, '2', 'PATIENT discharged him/herself or was discharged by a relative or advocate', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (312, 14, '3', 'PATIENT discharged by mental health review tribunal, Home Secretary or Court', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (313, 14, '4', 'PATIENT died', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (314, 14, '5', 'Stillbirth', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (315, 14, '6', 'PATIENT discharged him/herself *', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (316, 14, '7', 'PATIENT discharged by a relative or advocate *', 'SYSTEM', '2018-01-01 01:00:00');
 
 
 19	Usual place of residence unless listed below, for example, a private dwelling whether owner occupied or owned by Local Authority, housing association or other landlord. This includes wardened accommodation but not residential accommodation where health care is provided. It also includes PATIENTS with no fixed abode.
+INSERT INTO Codes VALUES (316, 14, '7', 'PATIENT discharged by a relative or advocate *', 'SYSTEM', '2018-01-01 01:00:00');
 29	Temporary place of residence when usually resident elsewhere (includes hotel, residential Educational Establishment)
 30	Repatriation from high security psychiatric accommodation in an NHS Hospital Provider (NHS Trust or NHS Foundation Trust)
 37	Court
