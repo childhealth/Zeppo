fa72ac3c170dd76caee3095667d8d307f73a663f
childhealth <mike.kelly1@nhs.net>
2018-01-05 12:03:41 +0000

    Update ConditionsDiagnoses.sql

--- sql/ConditionsDiagnoses.sql
+++ sql/ConditionsDiagnoses.sql
@@ -30,4 +30,47 @@ INSERT INTO CodeLists VALUES (13, 'ConditionsDiagnoses', 'Category', 'SYSTEM', '
 
 /* Insert ConditionsDiagnoses related codes
  */
-INSERT INTO Codes VALUES (62, 12, '1', 'X', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (267, 12, '1', 'Rubella', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (268, 12, '2', 'Varicella', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (269, 12, '3', 'Group B streptococcus', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (270, 12, '4', 'Asymptomatic bacteriuria', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (271, 12, '5', 'Toxoplasmosis', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (272, 12, '8', 'Tuberculosis', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (273, 12, '9', 'Cytomegalovirus', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (274, 12, '10', 'Parvovirus', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (275, 12, '11', 'Malaria', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (276, 12, '13', 'Cardiac Disease', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (277, 12, '14', 'Renal Disease', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (278, 12, '15', 'Mental Health Disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (279, 12, '16', 'Thromboembolic disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (280, 12, '17', 'Haematological disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (281, 12, '18', 'Central Nervous System (CNS) disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (282, 12, '19', 'Diabetes', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (283, 12, '20', 'Autoimmune disease', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (284, 12, '21', 'Cancer', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (285, 12, '22', 'Infectious hepatitis A', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (286, 12, '23', 'Hepatitis B', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (287, 12, '24', 'Hepatitis C', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (288, 12, '25', 'Endocrine Disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (289, 12, '26', 'Respiratory disease', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (290, 12, '27', 'Gastrointestinal disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (291, 12, '28', 'Musculoskeletal disorder', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (292, 12, '29', 'Gynaecological problems', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (293, 12, '100', 'Systemic Lupus Erythematosus (SLE)', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (294, 12, '101', 'Mother taking opioids', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (295, 12, '102', 'Thyrotoxicosis', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (296, 12, '103', 'Hypothyroidism', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (297, 12, '104', 'Anti - D positive', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (298, 12, '105', 'Immune thrombocytopenia', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (299, 12, '106', 'Vitamin D deficiency/osteoporosis', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (300, 12, '107', 'Myotonic dystrophy', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (301, 12, '108', 'Achondroplasia', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (302, 12, '109', 'Cytomegalovirus (CMV)', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (303, 12, '110', 'Chicken Pox', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (304, 12, '111', 'HIV', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (305, 12, '112', 'Anticonvulsant therapy', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (306, 12, '113', 'Alcohol abuse', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (307, 12, '114', 'Myasthenia Gravis', 'SYSTEM', '2018-01-01 01:00:00');
+
+INSERT INTO Codes VALUES (308, 13, '1', 'Problems and Issue', 'SYSTEM', '2018-01-01 01:00:00');
+INSERT INTO Codes VALUES (308, 13, '2', 'Diagnosis', 'SYSTEM', '2018-01-01 01:00:00');
